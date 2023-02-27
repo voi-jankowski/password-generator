3d5f1a40254b7fdf4bfd289ba64c94c4ee4f4a74
voi <wojtek.wacek.dola@gmail.com>
2023-02-27 18:21:49 +1100

    Added for loop in .js

--- script.js
+++ script.js
@@ -13,16 +13,26 @@ var includeSymbols = document.querySelector("#symbols");
 var copyPass = document.querySelector("#copy");
 
 
+// Add event listener to password range change.
+passLength.addEventListener("change", (event) => {
+  passLengthResult.innerText = event.target.value;
+});
+
+var lengthValue = passLengthResult.innerText;
+
+console.log(lengthValue);
 
 
 // Write the function to generate password and collect user input on password length, including uppercase or special characters.
 function writePassword(length, characters) {
+
   // Creating arrays of characters.
   var characters = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var symbols = "!@#$%^&*_-+=";
 
+  // Used conditions to include uppercase, numbers or symbols in line with the settings selections.
   if(includeUppercase.checked == true) {
     characters += uppercase;
   }
@@ -34,33 +44,32 @@ function writePassword(length, characters) {
   if(includeSymbols.checked == true) {
     characters += symbols;
   }
-
+  // Generating a random number and a random character based on that number.
   var randomNumber = Math.floor(Math.random() * characters.length);
   var randomCharacter = characters[randomNumber];
   console.log(randomCharacter);
 
+  var password = "";
 
- 
-  var password = generatePassword();
-  
+  for (i = 0; i < lengthValue; i++) {
+    password += randomCharacter;
+  }
 
   passwordText.value = password;
+ 
+  // var password = generatePassword();
+  
+  console.log(password);
+
+  return password;
+
 
 }
 
 
-// Add event listener to password range change.
-passLength.addEventListener("change", (event) => {
-  passLengthResult.innerText = event.target.value;
-});
 
-// Add event listener to generate button to create a string with the specified allowed characters selected in the options and of length specified in the options.
-generateBtn.addEventListener("click",  () => {
-  includeUppercase.checked ? (characters += uppercase) : "";
-  includeNumbers.checked ? (characters += numbers) : "";
-  includeSymbols.checked ? (characters += symbols) : "";
-  passwordText.value = writePassword(length.value, characters);
-});
+// Add event listener to generate button 
+generateBtn.addEventListener("click", writePassword);
 
 // Add event listener to copy to clipboard button:
 
